6b4811b661d76bfbf19fa829fc04833ad521ec35
anytao <1106217752@qq.com>
2017-08-29 22:50:45 +0800

    tab

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,13 +16,17 @@
             </v-sider>
             <v-layout>
                 <v-header :style="{ background: '#fff', padding: 0 }"></v-header>
-                <v-content :style="{ padding: '0 50px' }">
-                    <v-breadcrumb :style="{ margin: '12px 0' }">
+                <v-content :style="{ padding: '0 0px' }">
+                    <!-- <v-breadcrumb :style="{ margin: '12px 0' }">
                         <v-breadcrumb-item>Home</v-breadcrumb-item>
                         <v-breadcrumb-item href="">List</v-breadcrumb-item>
                         <v-breadcrumb-item href="">App</v-breadcrumb-item>
-                    </v-breadcrumb>
-                    <div style="padding: 24px; background: #fff; min-height: 360px;">Content</div>
+                    </v-breadcrumb> -->
+                    <div style="padding: 24px; background: #fff; min-height: 700px;">
+                        <v-tabs active-tab-key="tab1" type="editable-card" @add="onAdd" @remove="onRemove">
+                           <v-tab-pane v-for="tabPane in tabPanes" :tab-key="tabPane.key" :key="tabPane.key" :tab="tabPane.tab"></v-tab-pane>
+                       </v-tabs>
+                    </div>
                 </v-content>
                 <v-footer :style="{ textAlign: 'center' }">
                     Vue Antd Admin ©2017 Created by Anytao
@@ -81,7 +85,13 @@
                     name: 'File',
                     icon: 'file',
                     selected: true
-                }]
+                }],
+                   keyIndex: 1,
+                tabPanes: [
+                    {key: 'tab1', tab: '选项卡1', content: '选项卡1的内容'},
+                    {key: 'tab2', tab: '选项卡2', content: '选项卡2的内容'},
+                    {key: 'tab3', tab: '选项卡3', content: '选项卡3的内容'}
+                ]
             }
         },
         watch: {
@@ -89,5 +99,22 @@
                 console.log(val);
             },
         },
+        methods:{
+               onAdd() {
+                this.tabPanes.push({
+                    key: 'newTab' + this.keyIndex,
+                    tab: '新选项卡' + this.keyIndex,
+                    content: '新选项卡' + this.keyIndex++ + '内容'
+                });
+            },
+            onRemove(tabKey) {
+                for (let i = 0; i < this.tabPanes.length; i++) {
+                    if (this.tabPanes[i].key == tabKey) {
+                        this.tabPanes.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
     }
 </script>
